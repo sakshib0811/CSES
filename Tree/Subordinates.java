c1f83d039166985986cf565b5b2334577ec9295d
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2025-11-13 23:41:43 +0530

    Subordinates

--- Tree/Subordinates.java
+++ Tree/Subordinates.java
@@ -1,43 +1,47 @@
 package Tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Subordinates {
-    static final int MAX = 200005;
-    static List<List<Integer>> adj = new ArrayList<>();
-    static int[] subOrdinates = new int[MAX];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        List<List<Integer>> adj = new ArrayList<>(n + 1);
         for (int i = 0; i <= n; i++)
             adj.add(new ArrayList<>());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 2; i <= n; i++) {
-            int boss = sc.nextInt();
+            int boss = Integer.parseInt(st.nextToken());
             adj.get(boss).add(i);
         }
-        sc.close();
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
+
+        int[] subOrdinates = new int[n + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        List<Integer> order = new ArrayList<>();
+
         q.add(1);
         while (!q.isEmpty()) {
             int node = q.poll();
-            ans.add(node);
-            for (int i : adj.get(node)) {
-                q.add(i);
+            order.add(node);
+            for (int child : adj.get(node)) {
+                q.add(child);
             }
         }
-        for (int i = ans.size() - 1; i >= 0; i--) {
-            int temp = ans.get(i);
-            for (int j : adj.get(temp))
-                subOrdinates[temp] += subOrdinates[j] + 1;
-        }
-        for (int i = 1; i <= n; i++)
-            System.out.print(subOrdinates[i] + " ");
 
+        for (int i = order.size() - 1; i >= 0; i--) {
+            int node = order.get(i);
+            for (int child : adj.get(node)) {
+                subOrdinates[node] += subOrdinates[child] + 1;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(subOrdinates[i]).append(" ");
+        }
+        System.out.print(sb);
     }
 }
