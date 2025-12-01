3bd9a88c23e93788e534e932102c72ac1c42aac4
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2025-12-01 17:40:07 +0530

    Missing Coin Sum

---
+++ SearchingAndSorting/MissingCoinSum.java
@@ -0,0 +1,75 @@
+package SearchingAndSorting;
+
+import java.io.*;
+import java.util.*;
+
+public class MissingCoinSum {
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            long val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int n = fs.nextInt();
+        ArrayList<Long> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            arr.add(fs.nextLong());
+        Collections.sort(arr);
+        Long currSum = (long) 1;
+        for (int i = 0; i < n; i++) {
+            if (currSum < arr.get(i))
+                break;
+            currSum = currSum + arr.get(i);
+        }
+        out.println(currSum);
+        out.flush();
+
+    }
+
+}
