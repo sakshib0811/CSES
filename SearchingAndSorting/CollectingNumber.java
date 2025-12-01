66b4ce8e2e149dd9d345a11ea0590947043c0a50
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2025-12-01 18:35:28 +0530

    Collecting Numbers

---
+++ SearchingAndSorting/CollectingNumber.java
@@ -0,0 +1,74 @@
+package SearchingAndSorting;
+
+import java.io.*;
+import java.util.*;
+
+public class CollectingNumber {
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0)
+                    return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+            while ((c = read()) <= ' ')
+                if (c == -1)
+                    return -1;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            long val = c - '0';
+            while ((c = read()) > ' ')
+                val = val * 10 + (c - '0');
+            return val * sign;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        PrintWriter out = new PrintWriter(System.out);
+        int n = fs.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        long[] idx = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            arr.add(fs.nextInt());
+            idx[arr.get(i)] = i;
+        }
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            if (idx[i] > idx[i + 1])
+                ans++;
+        }
+        out.println(ans);
+        out.flush();
+    }
+}
